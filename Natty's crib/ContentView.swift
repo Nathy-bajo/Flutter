d5e02172c10e711d4d8162570708d9ce26048bf8
Nathy-bajo <bajon7680@gmail.com>
2022-04-26 11:27:46 +0100

    Initial Commit

---
+++ Natty's crib/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Natty's crib
+//
+//  Created by Nathaniel Bajo on 26/04/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Natty's crib/Natty_s_cribApp.swift
@@ -0,0 +1,17 @@
+//
+//  Natty_s_cribApp.swift
+//  Natty's crib
+//
+//  Created by Nathaniel Bajo on 26/04/2022.
+//
+
+import SwiftUI
+
+@main
+struct Natty_s_cribApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
